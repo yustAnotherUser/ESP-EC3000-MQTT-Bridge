5d8b455249c96d2c93c58b64af32d3ca205d0dea
yustAnotherUser <56141894+yustAnotherUser@users.noreply.github.com>
2025-04-17 15:23:40 +0200

    Create wifidata.h

---
+++ wifidata.h
@@ -0,0 +1,7 @@
+// WiFi and MQTT settings
+const char* ssid = "";            // WLAN Name
+const char* password = "";        // WLAN Password
+const char* mqtt_server = "192.168.0.254";
+const int mqtt_port = 1883;
+const char* mqtt_user = "";      // MQTT Server Username
+const char* mqtt_password = "";  // MQTT Server Password
